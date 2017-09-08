567351acd6f3e3d3a1b5efed06cfa90e08fc8882
Fabrizio Nasi <fnasi@golin.com>
2017-09-08 11:45:50 +0100

    chore(): import intl provider

--- assets/react/app.js
+++ assets/react/app.js
@@ -3,16 +3,20 @@ import { render } from 'react-dom';
 import { Router, Route, IndexRoute } from 'react-router';
 import { Provider } from 'react-redux';
 import store, { history } from './redux/store';
+import { IntlProvider } from 'react-intl';
 
 import App from './components/App/App';
 import LandingPageContainer from './components/LandingPage/LandingPageContainer';
 
 render((
   <Provider store={store}>
-    <Router history={history} onUpdate={() => window.scrollTo(0, 0)}>
-      <Route path="/" component={App}>
-        <IndexRoute component={LandingPageContainer} />
-      </Route>
-    </Router>
+    <IntlProvider locale="en" messages={store.getState().intl.messages.en}>
+      <Router history={history} onUpdate={() => window.scrollTo(0, 0)}>
+        <Route path="/" component={App}>
+          <IndexRoute component={LandingPageContainer} />
+        </Route>
+      </Router>
+    </IntlProvider>
   </Provider>
-), document.getElementById('root'));
+  ), document.getElementById('root')
+);
