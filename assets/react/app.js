f8dc8f51a342a8000e007926b13bb7336aa51f51
Fabrizio Nasi <fnasi@golin.com>
2017-09-11 11:12:58 +0100

    chore(): render the routes when window loads

--- assets/react/app.js
+++ assets/react/app.js
@@ -1,22 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Router, Route, IndexRoute } from 'react-router';
-import { Provider } from 'react-redux';
-import store, { history } from './redux/store';
-import { IntlProvider } from 'react-intl';
+import AppRoutes from './components/AppRoutes/AppRoutes';
 
-import App from './components/App/App';
-import LandingPageContainer from './components/LandingPage/LandingPageContainer';
-
-render((
-  <Provider store={store}>
-    <IntlProvider locale="en" messages={store.getState().intl.messages.en}>
-      <Router history={history} onUpdate={() => window.scrollTo(0, 0)}>
-        <Route path="/" component={App}>
-          <IndexRoute component={LandingPageContainer} />
-        </Route>
-      </Router>
-    </IntlProvider>
-  </Provider>
-  ), document.getElementById('root')
-);
+window.onload = () => {
+  render((
+    <AppRoutes />
+  ), document.getElementById('root'));
+};
