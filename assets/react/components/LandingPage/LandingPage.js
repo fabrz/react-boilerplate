a66184568e9476bd2b08c680e20c729125e6589f
Fabrizio Nasi <fnasi@golin.com>
2017-10-03 11:12:37 +0100

    chore(): SVGinline no longer works with React16, we’ll need to use a different package

--- assets/react/components/LandingPage/LandingPage.js
+++ assets/react/components/LandingPage/LandingPage.js
@@ -1,30 +1,12 @@
 import React from 'react';
 import cssModules from 'react-css-modules';
 import classNames from 'classnames';
-import SVGInline from 'react-svg-inline';
 
 import { getImage } from '../../helpers/general';
 import styles from './LandingPage.scss';
 
 const LandingPage = () => (
-  <div styleName={classNames('landing')}>
-    <div>
-      <ul>
-        <li>
-          <SVGInline svg={getImage('./svg/react.svg')} />
-        </li>
-        <li>
-          <SVGInline svg={getImage('./svg/redux.svg')} />
-        </li>
-        <li>
-          <SVGInline svg={getImage('./svg/es6.svg')} />
-        </li>
-        <li>
-          <SVGInline svg={getImage('./svg/babel.svg')} />
-        </li>
-      </ul>
-    </div>
-  </div>
+  <div styleName={classNames('landing')}></div>
 );
 
 export default cssModules(LandingPage, styles, { allowMultiple: true });
